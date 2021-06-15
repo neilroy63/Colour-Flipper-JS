f3fc26264286a51f03f52245b41945dcf8b5d094
Neil <neil.roy63@gmail.com>
2021-06-15 23:01:52 +1000

    fix/updated app js code

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,8 @@ btn.addEventListener("click", function () {
   // get random number between 0 - 3
   const randomNumber = 2;
   document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
   //   console.log(document.body);
 });
+
+function getRandomNumber() {}
